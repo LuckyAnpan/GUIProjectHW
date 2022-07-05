e543eefebb86661e390661b90832007c73ec8d43
Pimpawan <pimpawan.bo@gmail.com>
2022-07-05 11:42:54 +0700

    Add project

---
+++ GUIProject/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUIProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int intIncome = int.Parse(txtIncome.Text);
+            int intExpenses = int.Parse(txtExpenses.Text);
+            int intPrice = int.Parse(txtPrice.Text);
+
+            int result = intPrice / (intIncome - intExpenses);
+
+            txtDay.Text = result.ToString();
+        }
+    }
+}
